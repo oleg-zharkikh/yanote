dd73c9425ff912e92d50fe7e7338db0adde6920e
Oleg Zharkikh <ozharkikh@yandex.ru>
2025-07-01 21:44:45 +0300

    добавлены тесты pytest.

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,86 @@
+# test_routes.py
+from http import HTTPStatus
+import pytest
+from django.urls import reverse
+from notes.models import Note
+from pytest_lazy_fixtures import lf
+from pytest_django.asserts import assertRedirects
+
+
+def test_home_availability_for_anonymous_user(client):
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+# авторизированный клиент
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(not_author_client, name):
+    url = reverse(name)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_author(author_client, name, note):
+    url = reverse(name, args=(note.slug,))
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    [
+        (lf('not_author_client'), HTTPStatus.NOT_FOUND),
+        (lf('author_client'), HTTPStatus.OK)
+    ],
+) 
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, note, expected_status
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:detail', lf('slug_for_args')),
+        ('notes:edit', lf('slug_for_args')),
+        ('notes:delete', lf('slug_for_args')),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ),
+)
+def test_redirects(client, name, args):
+    login_url = reverse('users:login')
+    # Теперь не надо писать никаких if и можно обойтись одним выражением.
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    # Ожидаем, что со всех проверяемых страниц анонимный клиент
+    # будет перенаправлен на страницу логина:
+    assertRedirects(response, expected_url)
